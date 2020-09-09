88e4d9dfd85182108559e72a7a7f1f3d5f28659e
saj96n <saj96n@gmail.com>
2020-09-09 12:40:05 +0200

    Add loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import './styles.css';
+import { CircularProgress } from '@material-ui/core';
 
 const Loader = (props) => {
   return (
     <div className="loader-container loader" {...props}>
-      <h3 className="loader-content">Loading..</h3>
+      {/* <h3 className="loader-content">Loading..</h3> */}
+      <CircularProgress/>
     </div>
   );
 };
