cac01febccc3ccd8297c65f0e581b8f6e0009203
saj96n <saj96n@gmail.com>
2020-10-29 16:31:18 +0100

    Fix debug checkbox

--- src/pages/Settings/Settings.js
+++ src/pages/Settings/Settings.js
@@ -4,15 +4,21 @@ import {
   FormControlLabel,
   TextField,
   Button,
+  Box,
+  Grid,
 } from "@material-ui/core";
 import React from "react";
-import { useForm } from "react-hook-form";
+import { Controller, useForm } from "react-hook-form";
 import { connect } from "react-redux";
 import { compose } from "redux";
 import { editUser } from "../../store/actions/userActions";
 
 const Settings = ({ auth, editUser }) => {
-  const { register, handleSubmit } = useForm();
+  const { register, handleSubmit, control } = useForm({
+    defaultValues: {
+      debug: auth.me.settings?.debug,
+    },
+  });
 
   const onSubmit = (data) => editUser(auth.me.id, { settings: data });
 
@@ -20,26 +26,32 @@ const Settings = ({ auth, editUser }) => {
     <>
       <Typography variant="h5">Settings</Typography>
       {auth.me && (
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormControlLabel
-            control={
-              <Switch
-                inputRef={register}
-                // checked={auth.me.settings?.debug}
-                name="debug"
-              />
-            }
-            label="Debug"
-          />
-          <TextField
-            inputRef={register}
-            defaultValue={auth.me.settings?.debugId}
-            name="debugId"
-          />
-          <Button type="submit" variant="contained">
-            Save
-          </Button>
-        </form>
+        <Box>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <Controller
+              name="debug"
+              control={control}
+              render={(props) => (
+                <FormControlLabel
+                  control={
+                    <Switch
+                      checked={props.value}
+                      onChange={(e) => props.onChange(e.target.checked)}
+                    />
+                  }
+                  label="Debug"
+                />
+              )}
+            />
+            <TextField
+              inputRef={register}
+              defaultValue={auth.me.settings?.debugId}
+              name="debugId"
+            />
+            <Button type="submit" variant="contained">
+              Save
+            </Button>
+          </form>
       )}
     </>
   );
