5c0f2d579d9da5aa46240a44fbc23d56ee38f319
saj96n <saj96n@gmail.com>
2020-11-09 17:49:36 +0100

    Add websocket reducer

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,16 +1,17 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import authReducer from './authReducer';
-import followersReducer from './followersReducer';
-import userReducer from './userReducer';
-import syncReducer from './syncReducer';
-import navReducer from './navReducer';
-// import websocketReducer from './websocketReducer';
+import authReducer from "./authReducer";
+import followersReducer from "./followersReducer";
+import userReducer from "./userReducer";
+import syncReducer from "./syncReducer";
+import navReducer from "./navReducer";
+import websocketReducer from "./websocketReducer";
 
 export default combineReducers({
   auth: authReducer,
   followers: followersReducer,
   nav: navReducer,
   sync: syncReducer,
-  user: userReducer
+  user: userReducer,
+  websocket: websocketReducer,
 });
